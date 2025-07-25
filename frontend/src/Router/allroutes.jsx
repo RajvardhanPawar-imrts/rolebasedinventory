6edc3cbb164cde822a69c961e54028d298c119ea
RajvardhanPawar-imrts <rajvardhan.p@introspectivemarketresearch.com>
2025-07-25 10:10:16 +0530

    latest commit

--- frontend/src/Router/allroutes.jsx
+++
@@ -1,9 +0,0 @@
-export const all_routes = {
-  home: "/",
-  unauth401: "/unauthorized",
-  dashboard: "/dashboard",
-  login: "/login",
-  register: "/register",
-  sales: "/sales",
-  roles: "/roles",
-};
